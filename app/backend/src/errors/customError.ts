5a870b914bb0059e1e042abf06b7107258103aa8
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-15 15:02:33 -0300

    feat: file customError created

---
+++ app/backend/src/errors/customError.ts
@@ -0,0 +1,12 @@
+
+class CustomError extends Error {
+  constructor(
+    public status: number,
+    public message: string
+  ){
+    super(message);
+    this.status = status;
+  }
+}
+
+export default CustomError
