c82b5faa6a513f01a292b011a51f99ec9daa2c7d
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-17 19:09:06 -0300

    recycle:refactor:add CustomError 'two equal teams' req.25

--- app/backend/src/MSC/service/MatchesService.ts
+++ app/backend/src/MSC/service/MatchesService.ts
@@ -1,6 +1,9 @@
+import CustomError from '../../errors/customError';
 import MatchesModel from '../../database/models/Match';
 import TeamModel from '../../database/models/Team';
 import IMatch from '../../Interface/IMatch';
+// import IMatchResponse from '../../Interface/IMatchResponse';
+// import TeamsService from './TeamsService';
 
 // const implementada para incluir as keys teamHome e teamAway no resultTeams, raciocinio desenvolvido com a ajuda da colega Elaine Costa;
 const includeSequelize = [{
@@ -16,17 +19,29 @@ const includeSequelize = [{
 
 class MatchesService {
   public model = MatchesModel;
+  // public teamsService : TeamsService;
+
+  // constructor() {
+  //   this.teamsService = new TeamsService();
+  // }
 
   public getMatchAll = async ():Promise<IMatch[]> => {
     const resultTeams = await this.model.findAll({ include: includeSequelize });
     return resultTeams as IMatch[];
   };
 
-  public create = async ({
-    homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress }: IMatch): Promise<IMatch> => {
-    const resultCreate = await this.model.create({
-      homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress });
-    return resultCreate as IMatch;
+  public create = async (match: IMatch): Promise<IMatch> => {
+    const resultCreate = await this.model.create({ ...match });
+    if (match.homeTeam === match.awayTeam) {
+      throw new CustomError(401, 'It is not possible to create a match with two equal teams');
+    }
+
+    // const checkHomeTeam = await this.model.findByPk(match.homeTeam);
+    // if (checkHomeTeam) {
+    //    throw new CustomError()
+    // }
+    // console.log('matchService', resultCreate);
+    return resultCreate;
   };
 
   public finishMatch = async (id: number): Promise<number> => {
