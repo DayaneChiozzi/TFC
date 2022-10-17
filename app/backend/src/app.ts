a1ca7c2e0657a656b0c30aa79a21fe807e983863
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-17 09:51:30 -0300

    recycle:refactor: add teams route, req.15

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,9 +2,11 @@ import * as express from 'express';
 import tokenValidate from './middlewares/TokenValidation';
 import LoginValidation from './middlewares/loginValidation';
 import LoginController from './MSC/controllers/loginController';
+import TeamsController from './MSC/controllers/teamsController';
 
 const loginController = new LoginController();
 const loginValidation = new LoginValidation();
+const teamsController = new TeamsController();
 
 class App {
   public app: express.Express;
@@ -18,6 +20,7 @@ class App {
     this.app.get('/', (req, res) => res.json({ ok: true }));
     this.app.post('/login', loginValidation.loginVerify, loginController.login);
     this.app.get('/login/validate', tokenValidate, loginController.getRoleUser);
+    this.app.get('/teams', teamsController.getTeams);
   }
 
   private config():void {
