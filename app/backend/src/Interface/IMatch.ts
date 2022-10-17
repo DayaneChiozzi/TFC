362104ec581b519911291770567421374f75d2e6
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-17 11:56:31 -0300

    sparkles:feat: match interface created

---
+++ app/backend/src/Interface/IMatch.ts
@@ -0,0 +1,14 @@
+import ITeam from './ITeam';
+
+interface IMatch {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean | number;
+  teamHome?: ITeam;
+  teamAway?: ITeam;
+}
+
+export default IMatch;
